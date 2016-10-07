64594b629ebbdc9255e93bd0891bef4e9b4beb15
alexberry721 <alexberry721@gmail.com>
2016-10-07 08:53:46 -0700

    messy but working solution

--- Book Exercise 6.43 - Check Substrings/main.cpp
+++ Book Exercise 6.43 - Check Substrings/main.cpp
@@ -1,15 +1,93 @@
-//
-//  main.cpp
-//  Book Exercise 6.43 - Check Substrings
-//
-//  Created by ax on 10/5/16.
-//  Copyright © 2016 COMP130. All rights reserved.
-//
+// main.cpp
+// Reverse Numbers
+// Created by ax on 9/28/16.
+// Copyright © 2016 COMP130. All rights reserved.
 
-#include <iostream>
+#include<iostream>
+#include<string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool check_length(string s1, string s2) {
+    
+    bool long_enough = 0;
+    
+    if (!(s1.length() > s2.length()))
+    {
+        long_enough = 1;
+    }
+    
+    return long_enough;
+}
+
+int find_first_position(string s1, string s2) {
+    
+    char first_char_s1 = s1.at(0);
+    int return_val = -1;
+    string check_match_seqence = "";
+    
+    cout << "looking for match to letter \"" << first_char_s1 << "\" in second string." << endl;
+    
+    for (int i = 0; i < s2.length() - s1.length(); i++)
+    {
+        if (first_char_s1 == s2.at(i)) {
+            cout << "Found a first character match at position " << i + 1 << " in the second string." << endl;
+            return_val = i;
+            
+            // run another function to for loop check the next s1 length of s2
+            
+            cout << "running next checker" << endl;
+            
+            for (int j = 0; j < s1.length(); j++) {
+                cout << "s2.j: " << s2.at(i + j) << endl;
+                check_match_seqence += s2.at(i + j);
+            }
+            
+        }
+        
+    }
+    
+    cout << "string s1: " << s1 << endl;
+    cout << "check match sequence: " << check_match_seqence << endl;
+    
+    cout << "checking if s1 matches check match sequence... " << endl;
+    
+    if (s1 == check_match_seqence) {
+        cout << "we found a match!" << endl;
+    }
+    else {
+        cout << "boo, no match found!" << endl;
+    }
+        
+    
+    return return_val;
+    
+}
+
+int main() {
+    
+    string s1 = " ";
+    string s2 = " ";
+    
+    cout << "Enter string 1: ";
+    
+    getline(cin, s1);
+    
+    cout << "Enter string 2: ";
+    
+    getline(cin, s2);
+    
+    if (check_length(s1, s2) == 0) {
+        cout << "First string is longer than the second string, -1" << endl;
+    }
+    else {
+        cout << "We can work with this! ";
+        
+        cout << find_first_position(s1, s2) << endl;
+    }
+    
+    
+    
     return 0;
+    
 }
